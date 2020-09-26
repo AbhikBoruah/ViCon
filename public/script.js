d9c06cede40d5d2c72c008f7db44a347cb06d66a
WebDeV <webdev@Abhiks-MacBook-Air.local>
2020-09-26 11:57:24 +0530

    Messagin Option Enabled

--- public/script.js
+++ public/script.js
@@ -57,3 +57,24 @@ const addVideoStream = function(video, stream){
   })
   videoGrid.append(video)
 }
+
+let text = $('input')
+
+
+$('html').keydown((e) =>{
+  if(e.which == 13 & text.val().length != 0) {
+    console.log(text.val())
+    socket.emit('message', text.val());
+    text.val('')
+  }
+})
+
+socket.on('createMessage', message =>{
+  $('ul').append(`<li class="message"><b>user</b><br/>${message}</li>`);
+  scrollToBottom()
+})
+
+const scrollToBottom = () =>{
+  let d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
