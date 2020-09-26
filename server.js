5f0575f87e20d3a5b947a64ee7ed9d2fa93d9a95
WebDeV <webdev@Abhiks-MacBook-Air.local>
2020-09-26 12:32:26 +0530

    Port.env file

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ io.on('connection', socket => {
 
 
 
-server.listen(3030, function(req,res){
+server.listen(process.env.PORT  || 3030, function(req,res){
     console.log("Server started at port 3030")
 });
