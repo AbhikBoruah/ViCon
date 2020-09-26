fc7aafa30711ebc4f85cfbd6a297e4dc5155152a
WebDeV <webdev@Abhiks-MacBook-Air.local>
2020-09-26 12:16:51 +0530

    Added Mute Unmute/ StopPlay

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ const peers = {}
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
   }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream)
@@ -77,4 +77,61 @@ socket.on('createMessage', message =>{
 const scrollToBottom = () =>{
   let d = $('.main__chat_window');
   d.scrollTop(d.prop("scrollHeight"));
+}
+
+
+//mute video
+const muteUnmute = () =>{
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  }else{
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+
+const playStop = () =>{
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getVideoTracks()[0].enabled= false;
+    setPlayVideo();
+  }else{
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled =true;
+  }
+
+}
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
 }
